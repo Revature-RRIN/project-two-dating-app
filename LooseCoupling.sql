78c8c2e0723c0d0db2e3ecdce3c6b7a186a70b13
ikamlet <ikamlet@gitlab.com>
2020-03-09 19:50:21 -0400

    newest schema 7:50pm

--- LooseCoupling.sql
+++ LooseCoupling.sql
@@ -1,9 +1,11 @@
+drop table user_answer cascade constraints;
 drop table answers cascade constraints;
+drop table questions cascade constraints;
 drop table messages cascade constraints;
 drop table status cascade constraints; 
 drop table matches cascade constraints;
-drop table matchseeker cascade constraints;
-drop table admini cascade constraints;
+--drop table matchseeker cascade constraints;
+--drop table admini cascade constraints;
 drop table users cascade constraints;
 
 
@@ -44,41 +46,67 @@ create table matches (
     male_id number (3),
     female_id number (3),
     status_id number (3),
-    constraint fk_male_id foreign key (male_id) references matchseeker (id),
-    constraint fk_female_id foreign key (female_id) references matchseeker (id)
+    constraint fk_male_id foreign key (male_id) references users (users_id),
+    constraint fk_female_id foreign key (female_id) references users (users_id)
 );
 
 create table status (
     status_id number (3) primary key,
     status_state varchar2 (20),
-    constraint fk_status foreign key (id) references matches (id)
+    constraint fk_status foreign key (status_id) references matches (matches_id)
 );
 
 create table messages (
-    massages_id number (4) primary key,
+    messages_id number (4) primary key,
     sender_id number (3),
     receiver_id number (3),
     remark varchar2 (300),
-    constraint fk_id foreign key (id) references matches (id),
-    constraint fk_sender_id foreign key (sender_id) references matchseeker (id),
-    constraint fk_receiver_id foreign key (receiver_id) references matchseeker (id) 
+    constraint fk_id foreign key (messages_id) references matches (matches_id),
+    constraint fk_sender_id foreign key (sender_id) references users (users_id),
+    constraint fk_receiver_id foreign key (receiver_id) references users (users_id) 
     --or should it be user-id to include admins??
 );
 
+create table questions (
+    question_id number (2) primary key,
+    question varchar2 (300)
+);
+
+
+--create table answers (
+--    answers_id number (3) primary key,
+--    answer_1 number (3),
+--    answer_2 number (3),
+--    answer_3 number (3),
+--    answer_4 number (3),
+--    cumulative number (3),
+--    constraint fk_answer_id foreign key (id) references matchseeker (id)
+--);
+
 create table answers (
     answers_id number (3) primary key,
-    answer_1 number (3),
-    answer_2 number (3),
-    answer_3 number (3),
-    answer_4 number (3),
-    cumulative number (3),
-    constraint fk_answer_id foreign key (id) references matchseeker (id)
+    which_question_id number (2),
+    response number (2),
+    constraint fk_which_question_id foreign key (which_question_id) references questions (question_id)
 );
+
+create table user_answer (
+    q_id number (2),
+    u_id number (3),
+    a_id number (2),
+    constraint fk_q_id foreign key (q_id) references questions (question_id),
+    constraint fk_u_id foreign key (u_id) references users (users_id),
+    constraint fk_a_id foreign key (a_id) references answers (answers_id)
+);
+
 
 create sequence users_seq start with 1 increment by 1 minvalue 1 cache 2;
 create sequence matches_seq start with 1 increment by 1 minvalue 1 cache 2;
 create sequence status_seq start with 1 increment by 1 minvalue 1 cache 2;
 create sequence messages_seq start with 1 increment by 1 minvalue 1 cache 2;
+create sequence questions_seq start with 1 increment by 1 minvalue 1 cache 2;
 create sequence answers_seq start with 1 increment by 1 minvalue 1 cache 2;
+create sequence user_answer_seq start with 1 increment by 1 minvalue 1 cache 2;
 
 commit;
+
--- LooseCoupling/src/main/java/beans/Questions.java
+++ LooseCoupling/src/main/java/beans/Questions.java
@@ -20,6 +20,7 @@ public class Questions {
 	private String question;
 	
 	
+	
 	public Questions() {
 		super();
 	}
@@ -28,14 +29,14 @@ public class Questions {
 		super();
 		this.questionId = questionId;
 	}
-	
-	
+
 	public Questions(Integer questionId, String question) {
 		super();
 		this.questionId = questionId;
 		this.question = question;
 	}
 
+	
 	public Integer getQuestionId() {
 		return questionId;
 	}
@@ -48,5 +49,44 @@ public class Questions {
 	public void setQuestion(String question) {
 		this.question = question;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((question == null) ? 0 : question.hashCode());
+		result = prime * result + ((questionId == null) ? 0 : questionId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Questions other = (Questions) obj;
+		if (question == null) {
+			if (other.question != null)
+				return false;
+		} else if (!question.equals(other.question))
+			return false;
+		if (questionId == null) {
+			if (other.questionId != null)
+				return false;
+		} else if (!questionId.equals(other.questionId))
+			return false;
+		return true;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Questions [questionId=" + questionId + ", question=" + question + "]";
+	}
 		
+	
+	
 }

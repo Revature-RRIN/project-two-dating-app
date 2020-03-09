2cfab4af8461dbf64d61587f84bac0225cbd7179
ikamlet <ikamlet@gitlab.com>
2020-03-09 12:19:24 -0400

    3/9 12:19pm utils added

---
+++ LooseCoupling/src/main/java/utils/ConnectionUtil.java
@@ -0,0 +1,46 @@
+package utils;
+
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class ConnectionUtil {
+	// modularization and singleton access to the connection to the database
+	// Singleton Connection Factory
+	private static ConnectionUtil cu = null;
+	private static Properties prop;
+	private ConnectionUtil() {
+		prop = new Properties();
+		try {
+			// Use the class loader to retrieve the properties file
+			// this reduces reliance on the file system.
+			InputStream dbProps = ConnectionUtil.class.getClassLoader()
+					.getResourceAsStream("database.properties");
+			prop.load(dbProps);
+		} catch(Exception e) {
+			LogUtil.logException(e, ConnectionUtil.class);
+		}
+	}
+	public static synchronized ConnectionUtil getConnectionUtil() {
+		if(cu== null)
+			cu = new ConnectionUtil();
+		return cu;
+	}
+	public Connection getConnection() {
+		Connection conn = null;
+		try {
+			// We have to register our driver class
+			Class.forName(prop.getProperty("driver"));
+			conn = DriverManager.getConnection(
+					prop.getProperty("url"), // the url of the db
+					prop.getProperty("usr"), // our username
+					prop.getProperty("psw") // our password
+					);
+		} catch(Exception e) {
+			LogUtil.logException(e, ConnectionUtil.class);
+		}
+		return conn;
+	}
+}
---
+++ LooseCoupling/src/main/java/utils/LogUtil.java
@@ -0,0 +1,29 @@
+package utils;
+
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.apache.log4j.Logger;
+
+public class LogUtil {
+	// modularization of exception handling in my app
+	public static void logException(Exception e,
+			@SuppressWarnings("rawtypes") Class c) {
+		Logger log = Logger.getLogger(c);
+		log.error(e.getClass()+": "+e.getMessage());
+		for(StackTraceElement s : e.getStackTrace()) {
+			log.warn(s.getLineNumber()+": "+s.getClassName());
+		}
+	}
+	public static void rollback(Exception e,
+			Connection conn,
+			@SuppressWarnings("rawtypes") Class c) {
+		LogUtil.logException(e, c);
+		try {
+			conn.rollback();
+		} catch (SQLException e1) {
+			LogUtil.logException(e1, c);
+		}
+	}
+}

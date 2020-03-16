2ddbb04b8292bd05f73e20c1545f5d76ec7192ee
noahgthomas <noaht@mail.usf.edu>
2020-03-16 01:43:45 -0400

    added a service, made classes folder

--- loose-coupling/src/app/Classes/app.module.ts
+++ loose-coupling/src/app/Classes/app.module.ts
@@ -2,22 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { NgModule } from '@angular/core';
-import { CustomMaterialModule } from './core/material.module';
-import { AppRoutingModule } from './core/app.routing.module';
+import { CustomMaterialModule } from '../core/material.module';
+import { AppRoutingModule } from '../core/app.routing.module';
 import {FormsModule} from '@angular/forms';
 import { FlexLayoutModule} from '@angular/flex-layout';
 
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { UserComponent } from './user/user.component';
-import { ProfileComponent } from './profile/profile.component';
+import { AppComponent } from '../app.component';
+import { LoginComponent } from '../login/login.component';
+import { UserComponent } from '../user/user.component';
+import { ProfileComponent } from '../profile/profile.component';
 import { HttpClientModule } from '@angular/common/http';
-import { RegisterComponent } from './register/register.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { QuestionsComponent } from './questions/questions.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegisterComponent } from '../register/register.component';
+import { WelcomeComponent } from '../welcome/welcome.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { QuestionsComponent } from '../questions/questions.component';
+import { DashboardComponent } from '../dashboard/dashboard.component';
 
 @NgModule({
   declarations: [

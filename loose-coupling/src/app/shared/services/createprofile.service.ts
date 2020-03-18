b0de9a3c0480a3826c6bb6ccc291dfbb06db7943
ikamlet <ikamlet@gitlab.com>
2020-03-18 18:58:30 -0400

    6:58pm

--- loose-coupling/src/app/shared/services/createprofile.service.ts
+++ loose-coupling/src/app/shared/services/createprofile.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from "@angular/core";
-import { UrlService } from '../services/url.service';
+import { UrlService } from 'src/app/shared/url.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Users } from '../users';
+import { Users } from 'src/app/shared/classes/users';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { Currentuser } from '../currentuser';
+
 
 @Injectable()
 export class ProfileService {

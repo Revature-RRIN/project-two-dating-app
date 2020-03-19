7dd83633f5d2ba2356bcd2a328f7824085d8d2cf
noahgthomas <noaht@mail.usf.edu>
2020-03-19 00:16:39 -0400

    added some css

--- loose-coupling/src/app/shared/nav-bar/nav-bar.component.ts
+++ loose-coupling/src/app/shared/nav-bar/nav-bar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../services/users.service';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
--- loose-coupling/src/app/welcome/welcome.component.ts
+++ loose-coupling/src/app/welcome/welcome.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
+  isLogClicked: boolean = false;
+  isSignClicked: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showLogin() {
+    this.isLogClicked = true;
+  }
+
+  showSignup()  {
+    this.isSignClicked = true;
   }
 
 }

6f53a78207e4ae0f0dfb6008feda272ec6f42ffb
noahgthomas <noaht@mail.usf.edu>
2020-03-19 21:12:15 -0400

    messages stuff

--- loose-coupling/src/app/app-routing.module.ts
+++ loose-coupling/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { SignupComponent } from './shared/signup/signup.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ProfileComponent } from './profile/profile.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MessagesComponent } from './messages/messages.component';
 
 const routes: Routes = [
 
@@ -12,7 +13,8 @@ const routes: Routes = [
   {path: "profile", component: ProfileComponent},
   {path: "", component: WelcomeComponent},
   {path: "signup", component: SignupComponent},
-  {path: "dashboard", component: DashboardComponent}
+  {path: "dashboard", component: DashboardComponent},
+  {path: "messages", component: MessagesComponent}
 
 ];
 

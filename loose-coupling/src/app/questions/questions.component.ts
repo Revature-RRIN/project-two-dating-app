bbc95e94b6a381a0dbbde225f511473418ad3ffe
Rami Alazzawi <Rami.alazzawi.ra@gmail.com>
2020-03-22 22:44:53 -0400

    questions& answers

--- loose-coupling/src/app/questions/questions.component.ts
+++ loose-coupling/src/app/questions/questions.component.ts
@@ -5,6 +5,8 @@ import { QuestionService } from '../shared/services/question.service';
 import { Questions } from '../shared/classes/questions';
 import { UsersService } from '../shared/services/users.service';
 import { Answers } from '../shared/classes/answers';
+import { Scores } from '../shared/classes/scores';
+import { ProfileService } from '../shared/services/createprofile.service';
 
 @Component({
   selector: 'app-questions',
@@ -13,35 +15,57 @@ import { Answers } from '../shared/classes/answers';
 })
 export class QuestionsComponent implements OnInit {
 
-  @Input() questions: Questions[];
-  answers  : Answers;
+  @Input() //questions: Questions[];
+  total : number=0;
+  scores1  : Scores;
+  scores2  : Scores;
+  scores3  : Scores;
   users: Users;
-  @Output() submitted = new EventEmitter<Answers>();
+  @Output() submitted = new EventEmitter<Users>();
 
-  constructor(private questionsServices: QuestionService,private router: Router,
-    private us : UsersService) { }
+  constructor(
+    private questionsServices: QuestionService,
+    private router: Router,
+    private us : UsersService, 
+    private ps:ProfileService) { }
 
   ngOnInit(): void {
 
-    if (!this.answers) {
-      this.answers = new Answers();
+    if (!this.scores1) {
+      this.scores1=new Scores();
+      this.scores2=new Scores();
+      this.scores3=new Scores();
   }
-      
-    this.users = this.us.getUser(); 
+  
+  
+  this.users = this.us.getUser();
+     
     
-    this.questionsServices.getQuestions().subscribe(
+    /*this.questionsServices.getQuestions().subscribe(
       questions => {
         this.questions = questions;
       }
-    );
+    );*/
     }
 
-  submit(): void {
-    this.questionsServices.addAnswers(this.answers).subscribe(
-      answers => {
-        this.answers = answers;
-        this.submitted.emit(answers);
-        this.router.navigate(["user"]);
+    submit(){
+      this.total= this.total+ parseInt(this.scores1.qonescore) ;
+      this.router.navigate(["questions"]);
+      console.log(this.total);
+    }
+
+    submit2(){
+      this.total= this.total+parseInt(this.scores2.qtwoscore);
+      this.router.navigate(["questions"]);
+      console.log(this.total);
+    }
+
+  submit3(): void {
+    this.ps.updateProfile(this.users).subscribe(
+       users => {
+        this.users = users;
+        this.submitted.emit(users);
+        this.router.navigate(["user"]); 
       }
     );
     //this.router.navigate(["user"]);
--- loose-coupling/src/app/shared/classes/answers.ts
+++ loose-coupling/src/app/shared/classes/answers.ts
@@ -1,9 +1,5 @@
 export class Answers  {
     answersId : number;
-    whichQuestion : number;
+    whichQuestionId : number;
     response : number;
-    answer1 :  [{title:"Italian", value:1},
-    {title:"Mexican" ,value:2},
-    {title:"Cheese burger", value:3},
-    {title:"Chocolate", value:4}]
 }
